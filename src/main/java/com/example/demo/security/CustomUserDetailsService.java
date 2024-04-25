ef59f5069f684a3ab92ebd179d3863092232156b
Vladyslav Zharkov <138609115+VVVladyslav@users.noreply.github.com>
2024-04-25 14:35:25 +0300

    Update CustomUserDetailsService.java

--- src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -17,7 +17,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findById(username) //////////////////////////////////////////
+        User user = userRepository.findById(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
 
         return new org.springframework.security.core.userdetails.User(
@@ -26,4 +26,3 @@ public class CustomUserDetailsService implements UserDetailsService {
                 Collections.emptyList());
     }
 }
-
