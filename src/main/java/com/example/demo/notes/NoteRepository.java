21dc39af5ac560dda391ae01340d8070ecfbde35
Vladyslav Zharkov <138609115+VVVladyslav@users.noreply.github.com>
2024-04-25 14:35:15 +0300

    Update NoteRepository.java

--- src/main/java/com/example/demo/notes/NoteRepository.java
+++ src/main/java/com/example/demo/notes/NoteRepository.java
@@ -7,8 +7,5 @@ import java.util.List;
 
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
-    //List<Note> getUserNotes(String username);
     List<Note> findByUserNotes(String username);
-
 }
-
